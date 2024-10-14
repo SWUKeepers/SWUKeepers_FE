d3ca8963c6836b7ad9ed374c29a678d81105bc1b
SaltySalt77 <hyna0707@gmail.com>
2024-10-14 16:06:11 +0900

    Refact: Add alert message for api response

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -3,20 +3,38 @@ import { useState } from 'react';
 
 const Root = () => {
   const [file, setFile] = useState<null | File>(null);
+
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+
+    if (!file) {
+      alert('파일을 선택해주세요.');
+      return;
+    }
+
     const formData = new FormData();
+    formData.append('file', file);
 
-    formData.append('file', file as File);
-
-    axios.post('http://localhost:8000/api/upload/', formData);
+    axios
+      .post('http://localhost:8000/api/upload/', formData)
+      .then((response) => console.log(response))
+      .then(() => {
+        alert('파일 업로드에 성공했습니다.');
+      })
+      .catch((error) => {
+        alert('파일 업로드에 실패했습니다.');
+        console.error(error);
+      });
   };
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    if (event.target.files) {
+    if (event.target.files && event.target.files.length > 0) {
       setFile(event.target.files[0]);
+    } else {
+      setFile(null); // 파일이 선택되지 않은 경우
     }
   };
+
   return (
     <form onSubmit={handleSubmit}>
       <input
