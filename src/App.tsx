1db02454d404e251e5bb8dc0b2db42f56510b7eb
SaltySalt77 <alexisna@naver.com>
2024-11-10 19:31:13 +0900

    Feat: Add toast component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,25 @@
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import './App.css';
 import Root from './pages/Root';
+import { useToastStore } from './states/useToastStore';
+import ToastAlert from './components/ToastAlert';
 
 function App() {
-  return <Root />;
+  const { toastProps, closeToast } = useToastStore();
+
+  return (
+    <Router>
+      <Routes>
+        <ToastAlert
+          severity={toastProps.severity}
+          open={toastProps.open}
+          message={toastProps.message}
+          closeToast={closeToast}
+        />
+        <Route path='/' element={<Root />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/states/useToastStore.ts
@@ -0,0 +1,27 @@
+import { create } from 'zustand';
+
+interface IToastProps {
+  severity: 'info' | 'error' | 'warning' | 'success';
+  message: string;
+  open: boolean;
+}
+
+interface IToastStore {
+  toastProps: IToastProps;
+  openToast: (toast: IToastProps) => void;
+  closeToast: () => void;
+}
+
+export const useToastStore = create<IToastStore>((set) => ({
+  toastProps: { severity: 'info', message: '', open: false },
+  openToast: (toast) =>
+    set({
+      toastProps: {
+        severity: toast.severity,
+        message: toast.message,
+        open: true,
+      },
+    }),
+  closeToast: () =>
+    set({ toastProps: { severity: 'info', message: '', open: false } }),
+}));
