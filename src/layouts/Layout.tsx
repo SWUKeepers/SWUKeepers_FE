8e318a7a127b699efd8fdc17dfb1427a82a8a828
SaltySalt77 <hyna0707@gmail.com>
2024-11-30 19:22:16 +0900

    Design: Add AppBar

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,7 +1,33 @@
-import { Container } from '@mui/material';
+import { AppBar, Container, Typography } from '@mui/material';
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
-  return <Container maxWidth='sm'>{children}</Container>;
+  return (
+    <>
+      <AppBar
+        position='static'
+        sx={{
+          backgroundColor: '#fff',
+          p: '0.5rem',
+          mb: '1rem',
+        }}
+      >
+        <Typography
+          variant='h6'
+          sx={{ color: 'primary.main', fontStyle: 'italic' }}
+        >
+          SWUKeepers
+        </Typography>
+      </AppBar>
+      <Container
+        maxWidth='sm'
+        sx={{
+          minWidth: '320px',
+        }}
+      >
+        {children}
+      </Container>
+    </>
+  );
 };
 
 export default Layout;
